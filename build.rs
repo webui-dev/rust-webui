99066702da7aa8dcb53bc47fb1da6d0632cf2f7d
SpikeHD <spikegdofficial@gmail.com>
2024-02-11 10:34:20 -0800

    fix: buidl for unix systems

--- build.rs
+++ build.rs
@@ -1,5 +1,10 @@
 fn main() {
     // WebUI static lib
+    #[cfg(not(target_os = "windows"))]
+    {
+      println!("cargo:rustc-link-search=native=./");
+    }
+
     println!("cargo:rustc-link-lib=webui-2-static");
 
     #[cfg(target_os = "windows")]
