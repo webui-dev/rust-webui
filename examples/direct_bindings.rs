64315d50acedf82765de3fc63e28897dcfd34014
SpikeHD <spikegdofficial@gmail.com>
2024-02-11 11:58:29 -0800

    fix: fmt + clippy

--- examples/direct_bindings.rs
+++ examples/direct_bindings.rs
@@ -1,10 +1,10 @@
 use webui_rs::webui::bindgen::*;
 
 fn main() {
-  // Scary! The bindgen exports are interfacing directly with the C library, so no guarantees!
-  unsafe {
-    let win: usize = webui_new_window();
-    webui_show(win, "<html>Hello World!</html>".as_ptr() as *const i8);
-    webui_wait();
-  }
+    // Scary! The bindgen exports are interfacing directly with the C library, so no guarantees!
+    unsafe {
+        let win: usize = webui_new_window();
+        webui_show(win, "<html>Hello World!</html>".as_ptr() as *const i8);
+        webui_wait();
+    }
 }
